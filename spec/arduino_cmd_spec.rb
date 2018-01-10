64e272dae73fa1d62856f9677113ea504dea2345
Ian Katz <ianfixes@gmail.com>
2018-01-10 12:42:20 -0500

    add test for the board-is-installed function

--- spec/arduino_cmd_spec.rb
+++ spec/arduino_cmd_spec.rb
@@ -13,5 +13,16 @@ RSpec.describe ArduinoCI::ArduinoCmd do
       expect(arduino_cmd.installation.cmd_path).not_to be nil
     end
   end
-end
 
+  context "board_installed?" do
+    arduino_cmd = ArduinoCI::ArduinoCmd.autolocate!
+    ArduinoCI::DisplayManager::instance.enable
+    it "Finds installed boards" do
+      expect(arduino_cmd.board_installed? "arduino:avr:uno").to be true
+    end
+
+    it "Doesn't find bogus boards" do
+      expect(arduino_cmd.board_installed? "eggs:milk:wheat").to be false
+    end
+  end
+end
