c89354dcfc553c89a361d5cea287db931f05fab4
Ian Katz <ianfixes@gmail.com>
2018-02-22 07:30:39 -0500

    fix unit tests -- all positive -- and config

--- spec/testsomething_unittests_spec.rb
+++ spec/testsomething_unittests_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 
 sampleproj_path = File.join(File.dirname(File.dirname(__FILE__)), "SampleProjects")
 
-RSpec.describe ArduinoCI::CppLibrary do
+RSpec.describe "TestSomething C++ unit tests" do
   cpp_lib_path = File.join(sampleproj_path, "TestSomething")
   cpp_library = ArduinoCI::CppLibrary.new(cpp_lib_path)
   context "cpp_files" do
@@ -12,9 +12,9 @@ RSpec.describe ArduinoCI::CppLibrary do
       expect(relative_paths).to match_array(testsomething_cpp_files)
     end
   end
+  config = ArduinoCI::CIConfig.default.from_example(cpp_lib_path)
 
   context "test" do
-    config = ArduinoCI::CIConfig.default
 
     after(:each) do |example|
       if example.exception
@@ -31,19 +31,20 @@ RSpec.describe ArduinoCI::CppLibrary do
       expect(test_files.empty?).to be false
     end
 
+    it "has some allowable test files" do
+      allowed_files = config.allowable_unittest_files(cpp_library.test_files)
+      expect(allowed_files.empty?).to be false
+    end
+
+
     # well this override is clunky as hell.
     # @todo smooth this out for external purposes
-    ArduinoCI::CIConfig.default.with_config(cpp_lib_path, ArduinoCI::CIConfig.default) do |config_path|
-      config = ArduinoCI::CIConfig.default.with_override(config_path)
-
-      test_files = config.allowable_unittest_files(cpp_library.test_files)
-      test_files.each do |path|
-        expected = path.include?("good")
-        it "tests #{File.basename(path)} expecting #{expected}" do
-          exe = cpp_library.build_for_test_with_configuration(path, [], config.gcc_config("uno"))
-          expect(exe).not_to be nil
-          expect(cpp_library.run_test_file(exe)).to eq(expected)
-        end
+    test_files = config.allowable_unittest_files(cpp_library.test_files)
+    test_files.each do |path|
+      it "tests #{File.basename(path)} successfully" do
+        exe = cpp_library.build_for_test_with_configuration(path, [], config.gcc_config("uno"))
+        expect(exe).not_to be nil
+        expect(cpp_library.run_test_file(exe)).to_not be_falsey
       end
     end
   end
