ab604214eb9c7b6d641419b666d79e1c72ae4dbe
Ian Katz <ianfixes@gmail.com>
2018-04-25 10:44:48 -0400

    Initial and likely very broken windows CI commit

---
+++ lib/arduino_ci/arduino_cmd_windows.rb
@@ -0,0 +1,34 @@
+require "arduino_ci/host"
+require 'arduino_ci/arduino_cmd'
+
+module ArduinoCI
+
+  # Implementation of OSX commands
+  class ArduinoCmdWindows < ArduinoCmd
+    flag :get_pref,        "--get-pref"
+    flag :set_pref,        "--pref"
+    flag :save_prefs,      "--save-prefs"
+    flag :use_board,       "--board"
+    flag :install_boards,  "--install-boards"
+    flag :install_library, "--install-library"
+    flag :verify,          "--verify"
+
+    # run the arduino command
+    # @return [bool] whether the command succeeded
+    def _run_and_output(*args, **kwargs)
+      Host.run_and_output(*args, **kwargs)
+    end
+
+    # run the arduino command
+    # @return [Hash] keys for :success, :out, and :err
+    def _run_and_capture(*args, **kwargs)
+      Host.run_and_capture(*args, **kwargs)
+    end
+
+    def _lib_dir
+      File.join(ENV['userprofile'], "Documents", "Arduino", "libraries")
+    end
+
+  end
+
+end
