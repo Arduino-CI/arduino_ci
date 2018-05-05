9eaa85bb561cdae86bb9058b77d8c773ed9e8430
Tom Duff <tomduff@users.noreply.github.com>
2018-05-05 08:58:02 +0100

    Implemented windows forced install
    * Pulled package using open uri
    * Changed move to remove additional folder depth
    * Cleaned up extraction folder and zip after use
    * Added a few additional puts outputs to help see what is going on
    * Probably introduced a stack of rubocop errors

--- lib/arduino_ci/arduino_downloader_windows.rb
+++ lib/arduino_ci/arduino_downloader_windows.rb
@@ -2,6 +2,7 @@ require 'base64'
 require 'shellwords' # fingers crossed this works on win32
 require 'win32/registry'
 require "arduino_ci/arduino_downloader"
+require 'open-uri'
 
 module ArduinoCI
 
@@ -27,19 +28,27 @@ module ArduinoCI
     # (for logging purposes)
     # @return [string]
     def downloader
-      "wget"
+      "open-uri"
     end
 
     # Download the package_url to package_file
     # @return [bool] whether successful
     def download
-      powershell("(New-Object Net.WebClient).DownloadFile('#{package_url}', '#{package_file}')")
+      puts 'Downloading from ' + package_url
+      open(URI.parse(package_url)) do |url|
+        File.open(package_file, 'wb') { |file| file.write(url.read) }
+      end
     end
 
     # Move the extracted package file from extracted_file to the force_install_location
     # @return [bool] whether successful
     def install
-      powershell("Move-Item", extracted_file, self.class.force_install_location)
+      puts 'Installing to ' + self.class.force_install_location
+      # Move only the content of the directory
+      powershell("Move-Item", extracted_file + "\*", self.class.force_install_location)
+      # clean up the no longer required root extracted folder
+      puts 'Removing ' + package_file
+      powershell("Remove-Item", extracted_file)
     end
 
     # The local filename of the desired IDE package (zip/tar/etc)
@@ -58,7 +67,11 @@ module ArduinoCI
     # Extract the package_file to extracted_file
     # @return [bool] whether successful
     def extract
-      powershell("Expand-Archive", package_file, "-dest", extracted_file)
+      puts 'Extracting ' + package_file + " to " + extracted_file
+      powershell("Expand-Archive", "-Path", package_file, "-DestinationPath", extracted_file)
+      # clean up the no longer required zip
+      puts 'Removing ' + package_file
+      powershell("Remove-Item", package_file)
     end
 
     # The local file (dir) name of the extracted IDE package (zip/tar/etc)
@@ -78,11 +91,11 @@ module ArduinoCI
     # The executable Arduino file in an existing installation, or nil
     # @return [string]
     def self.existing_executable
-      arduino_reg = 'SOFTWARE\WOW6432Node\Arduino'
+      arduino_reg = 'SOFTWARE\WOW6432Node\Arduino1'
       Win32::Registry::HKEY_LOCAL_MACHINE.open(arduino_reg) do |reg|
         path = reg.read_s('Install_Dir')
-        puts 'Arduino Install Dir: ' + path
         exe = File.join(path, "arduino_debug.exe")
+        puts "Using existing exe located at " + exe
         return exe if File.exist? exe
       end
     rescue
@@ -93,6 +106,7 @@ module ArduinoCI
     # @return [string]
     def self.force_installed_executable
       exe = File.join(self.force_install_location, "arduino_debug.exe")
+      puts "using force installed exe located at " + exe
       return nil if exe.nil?
       exe
     end
