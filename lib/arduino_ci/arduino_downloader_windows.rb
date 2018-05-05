6df2412570f9dad6fca97e93b00e869c96098c51
Tom Duff <tomduff@users.noreply.github.com>
2018-05-05 09:20:18 +0100

    Rubocop again

--- lib/arduino_ci/arduino_downloader_windows.rb
+++ lib/arduino_ci/arduino_downloader_windows.rb
@@ -35,8 +35,8 @@ module ArduinoCI
     # @return [bool] whether successful
     def download
       puts 'Downloading from ' + package_url
-      # Turned off ssl verification 
-      open(URI.parse(package_url), {ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE}) do |url|
+      # Turned off ssl verification
+      open(URI.parse(package_url), ssl_verify_mode: 0) do |url|
         File.open(package_file, 'wb') { |file| file.write(url.read) }
       end
     end
