156ad939fccfd1e2108e7fb2af4ad69e10dacc6c
Ian Katz <ianfixes@gmail.com>
2018-09-13 16:07:27 -0400

    Fix symlinking on Windows

--- lib/arduino_ci/host.rb
+++ lib/arduino_ci/host.rb
@@ -39,15 +39,20 @@ module ArduinoCI
     end
 
     # if on windows, call mklink, else self.symlink
-    # https://stackoverflow.com/a/22716582/2063546
+    # @param [Pathname] old_path
+    # @param [Pathname] new_path
     def self.symlink(old_path, new_path)
-      return FileUtils.ln_s(old_path, new_path) unless RUBY_PLATFORM =~ /mswin32|cygwin|mingw|bccwin/
+      return FileUtils.ln_s(old_path.to_s, new_path.to_s) unless RUBY_PLATFORM =~ /mswin32|cygwin|mingw|bccwin/
 
+      # https://stackoverflow.com/a/22716582/2063546
       # windows mklink syntax is reverse of unix ln -s
       # windows mklink is built into cmd.exe
       # vulnerable to command injection, but okay because this is a hack to make a cli tool work.
-      _stdin, _stdout, _stderr, wait_thr = Open3.popen3('cmd.exe', "/c mklink #{new_path} #{old_path}")
-      wait_thr.value.exitstatus
+      orp = old_path.realpath.to_s.tr("/", "\\") # HACK DUE TO REALPATH BUG where it
+      np = new_path.to_s.tr("/", "\\")           # still joins windows paths with '/'
+
+      _stdout, _stderr, exitstatus = Open3.capture3('cmd.exe', "/C mklink /D #{np} #{orp}")
+      exitstatus.success?
     end
   end
 end
