27d10a5197872c3a42d16778433b3a31dc58bc8d
Håkon Løvdal <kode@denkule.no>
2019-02-03 21:49:30 +0100

    Add findSomething function

--- SampleProjects/DoSomething/do-something.cpp
+++ SampleProjects/DoSomething/do-something.cpp
@@ -4,3 +4,19 @@ int doSomething(void) {
   millis();  // this line is only here to test that we're able to refer to the builtins
   return 4;
 };
+
+static const struct something table[] = {
+	{ 1, "abc" },
+	{ 2, "xyz" },
+	{ 4, "123" },
+};
+
+const struct something *findSomething(int id) {
+	for (unsigned int i = 0; i < 3; i++) {
+		if (table[i].id == id) {
+			return &table[i];
+		}
+	}
+	return nullptr;
+}
+
--- SampleProjects/DoSomething/do-something.h
+++ SampleProjects/DoSomething/do-something.h
@@ -1,3 +1,11 @@
 #pragma once
 #include <Arduino.h>
 int doSomething(void);
+
+struct something {
+	int id;
+	const char *text;
+};
+
+const struct something *findSomething(int id);
+
