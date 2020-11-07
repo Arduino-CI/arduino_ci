6770338e39615567e674b51b024f2a140b7ddb8a
James Foster <github@jgfoster.net>
2020-11-07 10:08:36 -0800

    Update arduino_ci_remote.rb
    Fix typo referring to wong file.

--- exe/arduino_ci_remote.rb
+++ exe/arduino_ci_remote.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-puts "arduino_ci.rb is deprecated in favor of arduino_ci.rb."
+puts "arduino_ci_remote.rb is deprecated in favor of arduino_ci.rb."
 require_relative "arduino_ci.rb"
