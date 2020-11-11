cbccf6a714a07ee072a9204d6ab7550e546a5b71
Ian Katz <ianfixes@gmail.com>
2020-11-11 11:28:12 -0500

    Don't test SFR on non-AVR

--- SampleProjects/TestSomething/test/defines.cpp
+++ SampleProjects/TestSomething/test/defines.cpp
@@ -8,6 +8,7 @@ unittest(binary)
   assertEqual(100, B1100100);
 }
 
+#ifdef __AVR__
 #define DDRE      _SFR_IO8(0x02)
 
 unittest(SFR_IO8)
@@ -18,5 +19,6 @@ unittest(SFR_IO8)
   // this tests that directly
   auto foo = &DDRE;  // avoid compiler warning by using the result of an expression
 }
+#endif
 
 unittest_main()
