43f8d05ffd3d09bbc5643f895b713c6a4f68e4a4
James Foster <git@jgfoster.net>
2022-06-30 20:41:37 -0700

    This should do it!

--- SampleProjects/SharedLibrary/src/SharedLibrary.cpp
+++ SampleProjects/SharedLibrary/src/SharedLibrary.cpp
@@ -1,5 +1,5 @@
 #include "SharedLibrary.h"
 
 int main() {
-  return 0; // foo; // 'foo' was not declared in this scope
+  return foo; // 'foo' was not declared in this scope
 }
--- SampleProjects/SharedLibrary/test.sh
+++ SampleProjects/SharedLibrary/test.sh
@@ -3,9 +3,7 @@ cd SampleProjects/SharedLibrary
 bundle install
 bundle exec ensure_arduino_installation.rb
 bundle exec arduino_ci.rb --skip-examples-compilation
-status=$?
-echo "status=$status"
-if [ $status -ne 1 ]; then
+if [ $? -ne 1 ]; then
   exit 1
 fi
 exit 0
